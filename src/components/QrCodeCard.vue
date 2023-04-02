f86483f5e9554816f2f013f7888594db46cf5fb6
Italo Canturil <italo.cantur@gmail.com>
2023-04-01 23:10:34 -0300

    feat: add qrcode.js file to index

---
+++ src/components/QrCodeCard.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script lang="ts" setup>
+
+const props = defineProps({
+  errorConectionLevel: {
+    type: String,
+    default: 'L',
+    validator: (value: string) => ['L', 'M', 'Q', 'H'].indexOf(value) !== -1
+  },
+  data: { type: String, required: true }
+})
+</script>
